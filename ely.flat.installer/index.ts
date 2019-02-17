2c34c6ef437fd56bbb37cd8ae50a4a08d9154509
diego <masnimpu@gmail.com>
2019-02-18 00:15:00 +0300

    efi 1.1.1 полный рефракторинг системы

--- ely.flat.installer/index.ts
+++ ely.flat.installer/index.ts
@@ -3,18 +3,18 @@
 import * as child_process from "child_process";
 import * as path from "path";
 import process = require("process");
-import {cli} from "./cli/cli";
-import elyXLogger from "./core/elyXLogger";
-
-const logger = new elyXLogger({mainPrefix: "Builder"});
+import {cli} from "./bin/cli";
+import {efi} from "./bin/efi";
 
 //
 //  CLI Performing
 //
 
+efi.workingDirectory = path.resolve("./");
+
 const cliArgs = process.argv.slice(2);
 if (cliArgs.length > 0) {
-    cli(logger, cliArgs);
+    cli(cliArgs);
 } else {
     child_process.exec(path.resolve(__dirname + "/node_modules/.bin/electron") + " " +
         path.resolve(__dirname + "/gui.js"));
