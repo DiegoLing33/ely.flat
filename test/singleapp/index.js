cafd837688a679456984f1eaab3a00b72f17d948
diego <masnimpu@gmail.com>
2019-03-14 03:48:13 +0300

    Тестирование SelectField

--- test/singleapp/index.js
+++ test/singleapp/index.js
@@ -5,13 +5,16 @@ function ef(config) {
     config({});
 
     return vc => {
-        setTimeout(()=>{
+        setTimeout(() => {
             navigationBar().getProgressView().value(100);
         }, 1000);
 
-        const prog = new CircularProgressBarView();
-        prog.value(20);
-        vc.view.add(prog);
+        const sf = new SelectField({
+            items: [
+                "Male", "Female", "Middle", "Not known",
+            ]
+        });
+        vc.view.add(sf);
 
     };
 }
